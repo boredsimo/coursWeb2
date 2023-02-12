b01b22fadc9b281057e9c0e83ecaa950202e41a6
Dini Ahamada <dahamada@crosemont.qc.ca>
2023-02-11 21:52:45 -0500

    6 la méthode findAll

---
+++ ApplicationUtilisateur/src/java/com/dahamada/chezbio/model/dao/UtilisateurDao.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dahamada.chezbio.model.dao;
+
+import com.dahamada.chezbio.model.entities.Role;
+import com.dahamada.chezbio.model.entities.Utilisateur;
+import java.util.List;
+
+/**
+ *
+ * @author dahamada
+ */
+public interface UtilisateurDao {
+
+    List<Utilisateur> findAll();
+
+    Utilisateur findById(int id);
+
+    Utilisateur findByName(String nom);
+
+    Utilisateur findByEmail(String email);
+
+    Utilisateur findByNameRole(String nomRole);
+
+    List<Role> findAllRole();
+
+    List<Utilisateur> findAllByNameRole(String nomRole);
+
+    Utilisateur existsByEmailAndPassword(String email, String motDePasse);
+
+    boolean delete(int id);
+
+    boolean create(Utilisateur utilisateur);
+
+    boolean create(Utilisateur utilisateur, String nomRol);
+
+    boolean update(Utilisateur utilisateur);
+
+}
